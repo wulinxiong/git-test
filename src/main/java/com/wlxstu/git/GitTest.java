248bb8f0f82b740ecb6dc53c9ec6709b966ee50f
wlx <1031010320@qq.com>
2022-12-21 18:10:47 +0800

    first commit

---
+++ src/main/java/com/wlxstu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.wlxstu.git;
+
+/**
+ * @author 吴林雄
+ * @version 1.0
+ * @date 2022/12/21 18:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
